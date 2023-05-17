bd7a03974ad7e44a8f8eed12ad92dda059705d80
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-05-16 23:23:39 -0300

    Enhances signup component

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -2,13 +2,25 @@ import { SignUpForm } from "./SignUpForm";
 import { signUpValidations } from "./SignUpFormValidation";
 import { SignUpFormValues } from "./SignUpFormValues";
 import Title from "antd/es/typography/Title";
+import { createUserByEmailAndPassword } from "../../services/userService";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp: React.FC = () => {
-  const onSubmit = (values: SignUpFormValues) => {
+  const navigate = useNavigate();
+
+  const onSubmit = async (values: SignUpFormValues) => {
     console.log("Submitted values", values);
     const validationResult = signUpValidations(values);
+
     if (validationResult !== null) {
       console.log("Handling sign up");
+      try {
+        await createUserByEmailAndPassword(values.email, values.password);
+        console.log("User created!");
+        navigate("/login");
+      } catch (err) {
+        console.error("Error creating user: " + err);
+      }
     } else {
       alert(validationResult);
     }
